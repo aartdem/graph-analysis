09b4a0211c7be47f7482e736ffc8e88f70554599
Artem Rzhankov <rzhankov@mindbox.cloud>
2025-05-14 20:31:14 +0300

    feat: add graphblas to project

---
+++ src/lib/lagraph/boruvka_lagraph.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "common/mst_algorithm.hpp"
+
+namespace algos {
+    class BoruvkaLagraph : public MstAlgorithm {
+
+    public:
+        void load_graph(const std::filesystem::path &file_path) final;
+
+        std::chrono::milliseconds compute() final;
+
+        Tree get_result() final;
+
+    private:
+        void compute_();
+
+        std::unique_ptr<Tree> tree = nullptr;
+        uint64_t weight = 0;
+    };
+}
